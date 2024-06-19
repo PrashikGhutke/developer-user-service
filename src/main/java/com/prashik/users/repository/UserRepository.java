9ad1e94b775dbcba95925ae0e73746d7abe4b129
Prashik <prashikghutke@gmail.com>
2024-06-19 22:19:41 +0530

    Revert "User Service"
    This reverts commit 87ee1cc83b7d8356254d31844bacb0a219162e61.

--- src/main/java/com/prashik/users/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prashik.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prashik.users.model.User;
-
-public interface UserRepository extends JpaRepository<User, String>{
-
-}
